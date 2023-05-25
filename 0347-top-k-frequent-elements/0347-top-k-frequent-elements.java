0c447647aa3ff212cea66507d64064c7d212ef4f
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-25 17:29:56 +0530

    Time: 13 ms (83.31%), Space: 47.9 MB (35.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,55 +1,55 @@
-// // Priority Queue
-// // Time Complexity: nlogk, n = nums.length
-// // Space Complexity: n + k
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         Map<Integer, Integer> count = new HashMap<>();
-//         for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+// Priority Queue
+// Time Complexity: nlogk, n = nums.length
+// Space Complexity: n + k
+class Solution1 {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
 
-//         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
-//         for (int num : count.keySet()) {
-//             pq.offer(num);
-//             if (pq.size() > k)
-//                 pq.poll();
-//         }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
+        for (int num : count.keySet()) {
+            pq.offer(num);
+            if (pq.size() > k)
+                pq.poll();
+        }
 
-//         int i = 0;
-//         int[] result = new int[k];
-//         while (!pq.isEmpty())
-//             result[i++] = pq.poll();
-//         return result;
-//     }
-// }
+        int i = 0;
+        int[] result = new int[k];
+        while (!pq.isEmpty())
+            result[i++] = pq.poll();
+        return result;
+    }
+}
 
 // ----------------------------------------------------------------------------------------------------------------------------------------
 
-// // Bucket sort
-// // Time Complexity: n, n = nums.length
-// // Space Complexity: n + k
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         Map<Integer, Integer> count = new HashMap<>();
-//         for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+// Bucket sort
+// Time Complexity: n, n = nums.length
+// Space Complexity: n + k
+class Solution2 {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
 
-//         List<Integer>[] bucket = new ArrayList[nums.length + 1];
-//         for (int num : count.keySet()) {
-//             int frequency = count.get(num);
-//             if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
-//             bucket[frequency].add(num);
-//         }
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for (int num : count.keySet()) {
+            int frequency = count.get(num);
+            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
+            bucket[frequency].add(num);
+        }
 
-//         int idx = 0;
-//         int[] result = new int[k];
-//         for (int i = bucket.length - 1; i >= 0; i--) {
-//             if (bucket[i] == null) continue;
-//             for (int num : bucket[i]) {
-//                 result[idx++] = num;
-//                 if (idx == k) return result;
-//             }
-//         }
-//         return null;
-//     }
-// }
+        int idx = 0;
+        int[] result = new int[k];
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] == null) continue;
+            for (int num : bucket[i]) {
+                result[idx++] = num;
+                if (idx == k) return result;
+            }
+        }
+        return null;
+    }
+}
 
 // ----------------------------------------------------------------------------------------------------------------------------------------
 
