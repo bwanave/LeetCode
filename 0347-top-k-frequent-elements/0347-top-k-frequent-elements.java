b0572a8090776002518fa510201fbaef35dcc77d
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-25 17:25:26 +0530

    Time: 13 ms (83.31%), Space: 48.2 MB (8.40%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -21,30 +21,73 @@
 //     }
 // }
 
-// Bucket sort
-// Time Complexity: n, n = nums.length
-// Space Complexity: n + k
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+// // Bucket sort
+// // Time Complexity: n, n = nums.length
+// // Space Complexity: n + k
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         Map<Integer, Integer> count = new HashMap<>();
+//         for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+
+//         List<Integer>[] bucket = new ArrayList[nums.length + 1];
+//         for (int num : count.keySet()) {
+//             int frequency = count.get(num);
+//             if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
+//             bucket[frequency].add(num);
+//         }
+
+//         int idx = 0;
+//         int[] result = new int[k];
+//         for (int i = bucket.length - 1; i >= 0; i--) {
+//             if (bucket[i] == null) continue;
+//             for (int num : bucket[i]) {
+//                 result[idx++] = num;
+//                 if (idx == k) return result;
+//             }
+//         }
+//         return null;
+//     }
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+// Quick Select
+// Time Complexity: n, n = nums.length, in worst case it takes n^2
+// Space Complexity: n
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
 
-        List<Integer>[] bucket = new ArrayList[nums.length + 1];
-        for (int num : count.keySet()) {
-            int frequency = count.get(num);
-            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
-            bucket[frequency].add(num);
-        }
+        int[] arr = new int[count.size()];
+        int i = 0;
+        for (int num : count.keySet()) arr[i++] = num;
 
-        int idx = 0;
-        int[] result = new int[k];
-        for (int i = bucket.length - 1; i >= 0; i--) {
-            if (bucket[i] == null) continue;
-            for (int num : bucket[i]) {
-                result[idx++] = num;
-                if (idx == k) return result;
+        int pivot = quickSelect(0, arr.length - 1, arr, count, arr.length - k);
+        return Arrays.copyOfRange(arr, pivot, arr.length);
+    }
+
+    private int quickSelect(int left, int right, int[] arr, Map<Integer, Integer> count, int k) {
+        int j = left, pivot = right;
+        for (int i = left; i < right; i++) {
+            if (count.get(arr[i]) < count.get(arr[pivot])) {
+                swap(i, j, arr);
+                j++;
             }
         }
-        return null;
+
+        swap(j, pivot, arr);
+        if (k == j) return j;
+        else if (k > j) return quickSelect(j + 1, right, arr, count, k);
+        else return quickSelect(left, j - 1, arr, count, k);
+    }
+
+    private void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
+
