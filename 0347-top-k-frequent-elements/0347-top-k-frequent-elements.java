1043f955dc40ad4967ec3b3721f18819ba9d8a55
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-25 16:19:13 +0530

    Time: 11 ms (95.03%), Space: 47.7 MB (48.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,50 @@
-// Priority Queue
-// TC - nlogk, n = nums.length
+// // Priority Queue
+// // Time Complexity: nlogk, n = nums.length
+// // Space Complexity: n + k
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         Map<Integer, Integer> count = new HashMap<>();
+//         for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
+//         for (int num : count.keySet()) {
+//             pq.offer(num);
+//             if (pq.size() > k)
+//                 pq.poll();
+//         }
+
+//         int i = 0;
+//         int[] result = new int[k];
+//         while (!pq.isEmpty())
+//             result[i++] = pq.poll();
+//         return result;
+//     }
+// }
+
+// Bucket sort
+// Time Complexity: n, n = nums.length
+// Space Complexity: n + k
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
         for (int num : count.keySet()) {
-            pq.offer(num);
-            if (pq.size() > k)
-                pq.poll();
+            int frequency = count.get(num);
+            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
+            bucket[frequency].add(num);
         }
 
-        System.out.println(pq);
-
-        int i = 0;
+        int idx = 0;
         int[] result = new int[k];
-        while (!pq.isEmpty())
-            result[i++] = pq.poll();
-        return result;
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] == null) continue;
+            for (int num : bucket[i]) {
+                result[idx++] = num;
+                if (idx == k) return result;
+            }
+        }
+        return null;
     }
 }
