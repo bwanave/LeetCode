c77e939e382008e0be9c28bfbd2d4f209ae923bf
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-25 21:14:33 +0530

    Time: 2 ms (50.31%), Space: 53.7 MB (8.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,19 @@
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] prefixProduct = new int[nums.length];
         prefixProduct[0] = 1;
-        for (int i = 1; i < nums.length; i++) 
-            prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
-        
+        for (int i = 1; i < nums.length; i++) prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
+
         int[] sufixProduct = new int[nums.length];
         sufixProduct[nums.length - 1] = 1;
-        for (int i = nums.length - 2; i >= 0; i--) 
-            sufixProduct[i] = sufixProduct[i + 1] * nums[i + 1];
-        
+        for (int i = nums.length - 2; i >= 0; i--) sufixProduct[i] = sufixProduct[i + 1] * nums[i + 1];
+
         int[] result = new int[nums.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = prefixProduct[i] * sufixProduct[i];
-        }
+        for (int i = 0; i < result.length; i++) result[i] = prefixProduct[i] * sufixProduct[i];
         return result;
     }
 }
