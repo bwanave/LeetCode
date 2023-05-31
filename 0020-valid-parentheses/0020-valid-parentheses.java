720d133332d1527dfe1aaaee236ea8a31fae7a25
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-31 16:32:53 +0530

    Time: 2 ms (89.73%), Space: 40.9 MB (28.69%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = Map.of('{', '}', '(', ')', '[', ']');
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c))
+                stack.push(c);
+            else {
+                if (stack.isEmpty() || !map.get(stack.pop()).equals(c))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
