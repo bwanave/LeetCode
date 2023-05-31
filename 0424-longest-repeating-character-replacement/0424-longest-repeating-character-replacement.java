f4162d6cfdf4018c54b3c990320c987b4704519d
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-31 09:29:51 +0530

    Time: 32 ms (22.15%), Space: 44 MB (5.58%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int len = 0;
+        int maxRepeatingCharCount = 0;
+        int start = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            
+            maxRepeatingCharCount = Math.max(maxRepeatingCharCount, map.get(c));
+            if (end - start + 1 - maxRepeatingCharCount > k) {
+                c = s.charAt(start);
+                map.put(c, map.get(c) - 1);
+                start++;
+            }
+            
+            len = Math.max(len, end - start + 1);
+        }
+        return len;
+    }
+}
