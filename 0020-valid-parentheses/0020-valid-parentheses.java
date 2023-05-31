cf7518472021cf0ffcc74dbbd5557848b09c4076
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-31 16:34:05 +0530

    Time: 2 ms (89.73%), Space: 41.1 MB (6.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,12 +3,8 @@ class Solution {
         Map<Character, Character> map = Map.of('{', '}', '(', ')', '[', ']');
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (map.containsKey(c))
-                stack.push(c);
-            else {
-                if (stack.isEmpty() || !map.get(stack.pop()).equals(c))
-                    return false;
-            }
+            if (map.containsKey(c)) stack.push(c);
+            else if (stack.isEmpty() || !map.get(stack.pop()).equals(c)) return false;
         }
         return stack.isEmpty();
     }
