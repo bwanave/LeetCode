f70646ff75ff3ee92d63217b52b0a2472eb8d7fb
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-05-31 09:30:49 +0530

    Time: 31 ms (24.00%), Space: 43.7 MB (5.58%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -7,14 +7,14 @@ class Solution {
         for (int end = 0; end < s.length(); end++) {
             char c = s.charAt(end);
             map.put(c, map.getOrDefault(c, 0) + 1);
-            
+
             maxRepeatingCharCount = Math.max(maxRepeatingCharCount, map.get(c));
             if (end - start + 1 - maxRepeatingCharCount > k) {
                 c = s.charAt(start);
                 map.put(c, map.get(c) - 1);
                 start++;
             }
-            
+
             len = Math.max(len, end - start + 1);
         }
         return len;
