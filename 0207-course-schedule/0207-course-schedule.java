288f7b06d83f0db97555592bcef597eb57d07439
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-06-28 13:13:32 +0530

    Time: 5 ms (70.00%), Space: 44.4 MB (29.40%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> adjList = new HashMap<>();
-        for (int[] p : prerequisites) 
+        for (int[] p : prerequisites)
             adjList.computeIfAbsent(p[1], key -> new ArrayList<>()).add(p[0]);
-        
+
         int[] colors = new int[numCourses]; // 1: BLACK (Visited), 0: WHITE (Unvisited), -1: GRAY (Visiting)
         for (int course = 0; course < numCourses; course++) {
-            if (hasCycle(course, adjList, colors))
-                return false;
+            if (hasCycle(course, adjList, colors)) return false;
         }
         return true;
     }
-    
+
     private boolean hasCycle(int course, Map<Integer, List<Integer>> adjList, int[] colors) {
         if (colors[course] == 1) return false;
         if (colors[course] == -1) return true; // Found cycle
