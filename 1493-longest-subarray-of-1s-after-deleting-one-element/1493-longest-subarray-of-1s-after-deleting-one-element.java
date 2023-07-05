c559a9413692ee59dbe8bbd25fc06ff31be7bb8e
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-07-06 00:08:39 +0530

    Time: 2 ms (84.58%), Space: 53.1 MB (41.13%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,23 +1,13 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        // Number of zero's in the window.
+        int maxSize = 0;
         int zeroCount = 0;
-        int longestWindow = 0;
-        // Left end of the window.
         int start = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            zeroCount += (nums[i] == 0 ? 1 : 0);
-                          
-            // Shrink the window until the zero counts come under the limit.
-            while (zeroCount > 1) {
-                zeroCount -= (nums[start] == 0 ? 1 : 0);
-                start++;
-            }
-              
-            longestWindow = Math.max(longestWindow, i - start);
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] == 0) zeroCount++;
+            while (zeroCount > 1) if (nums[start++] == 0) zeroCount--;
+            maxSize = Math.max(maxSize, end - start + 1);
         }
-
-        return longestWindow;
+        return maxSize - 1;
     }
 }
