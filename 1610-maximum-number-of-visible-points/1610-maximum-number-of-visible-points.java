b74fc9a0ce9b554a90e1f5da75ffd24d9882bb1c
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-07-29 11:38:46 +0530

    Time: 125 ms (88.81%), Space: 76.7 MB (95.80%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.java
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
-        int samePoint = 0;
+    public int visiblePoints(List<List<Integer>> points, int allowedAngle, List<Integer> location) {
+        int samePoints = 0;
         List<Double> angles = new ArrayList<>();
         for (List<Integer> point : points) {
-            if (point.get(0) == location.get(0) && point.get(1) == location.get(1)) samePoint++;
+            if (point.get(0) == location.get(0) && point.get(1) == location.get(1)) samePoints++;
             else angles.add(getPolarAngle(point, location));
         }
-
         Collections.sort(angles);
-        List<Double> temp = new ArrayList<>(angles);
-        for (double a : angles) temp.add(a + 360);
-        angles = temp;
-
-        // Sliding window
-        int count = 0;
+        int n = angles.size();
+        for (int i = 0; i < n; i++) angles.add(angles.get(i) + 360);
+        
+        int visiblePoints = 0;
         int start = 0;
         for (int end = 0; end < angles.size(); end++) {
-            while (angles.get(end) - angles.get(start) > angle) 
+            while (angles.get(end) - angles.get(start) > allowedAngle)
                 start++;
-            count = Math.max(end - start + 1, count);            
+            visiblePoints = Math.max(visiblePoints, end - start + 1);
         }
-        return count + samePoint;
+        return visiblePoints + samePoints;
     }
-
+    
     private double getPolarAngle(List<Integer> point, List<Integer> location) {
         double radian = Math.atan2(point.get(1) - location.get(1), point.get(0) - location.get(0));
         double angle = radian * (180 / Math.PI);
