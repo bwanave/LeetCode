870319ca809f1a00beb0b2a063be698c0067b012
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-07-29 17:12:56 +0530

    Time: 11 ms (96.01%), Space: 42.7 MB (96.88%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.java
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -1,20 +1,20 @@
 class Solution {
     public int numSimilarGroups(String[] strs) {
-        int n = strs.length;
-        UnionFind uf = new UnionFind(n);
+        int groups = strs.length;
+        UnionFind uf = new UnionFind(strs.length);
         for (int i = 0; i < strs.length; i++) {
             for (int j = i + 1; j < strs.length; j++) {
-                if (hasEdge(strs[i], strs[j]) && uf.union(i, j))
-                    n--;
+                if (isSimilar(strs[i], strs[j]) && uf.union(i, j)) 
+                    groups--;
             }
         }
-        return n;
+        return groups;
     }
-
-    private boolean hasEdge(String str1, String str2) {
+    
+    private boolean isSimilar(String s1, String s2) {
         int mismatch = 0;
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) != str2.charAt(i)) mismatch++;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) mismatch++;
             if (mismatch > 2) return false;
         }
         return true;
@@ -22,30 +22,30 @@ class Solution {
 }
 
 class UnionFind {
-    private final int[] parent;
-    private final int[] rank;
-
-    UnionFind(int n) {
+    private int[] parent;
+    private int[] rank;
+    
+    public UnionFind(int n) {
         parent = new int[n];
-        rank = new int[n];
         for (int i = 0; i < n; i++) parent[i] = i;
+        rank = new int[n];
     }
-
-    private int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
+    
+    private int find(int node) {
+        if (parent[node] != node) parent[node] = find(parent[node]);
+        return parent[node];
     }
-
-    public boolean union(int x, int y) {
-        int p1 = find(x);
-        int p2 = find(y);
+    
+    public boolean union(int node1, int node2) {
+        int p1 = find(node1);
+        int p2 = find(node2);
         if (p1 == p2) return false;
-
+        
         if (rank[p1] > rank[p2]) parent[p2] = p1;
-        else if (rank[p1] < rank[p2]) parent[p1] = p2;
+        else if (rank[p2] > rank[p1]) parent[p1] = p2;
         else {
             parent[p2] = p1;
-            rank[p1] += rank[p2] + 1;
+            rank[p1]++;
         }
         return true;
     }
