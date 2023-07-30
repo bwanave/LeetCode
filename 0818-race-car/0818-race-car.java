3efb1e0dc0a752794289e98c347c5db948c4f8cc
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2023-07-30 15:37:25 +0530

    Time: 9 ms (77.51%), Space: 42.7 MB (68.24%) - LeetHub

--- 0818-race-car/0818-race-car.java
+++ 0818-race-car/0818-race-car.java
@@ -1,18 +1,19 @@
 class Solution {
+
     public int racecar(int target) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0, 1, 0}); // pos, speed, move
-        
+        queue.offer(new int[] { 0, 1, 0 }); // pos, speed, move
+
         while (!queue.isEmpty()) {
             int[] entry = queue.poll();
             int pos = entry[0], speed = entry[1], move = entry[2];
             if (pos == target) return move;
-            
+
             int nextPos = pos + speed, nextSpeed = speed * 2;
-            queue.offer(new int[] {nextPos, nextSpeed, move + 1});
-            
-            if ((nextPos > target && speed > 0) || (nextPos < target && speed < 0))
-                queue.offer(new int[] {pos, speed > 0 ? -1 : 1, move + 1});
+            queue.offer(new int[] { nextPos, nextSpeed, move + 1 });
+
+            if ((nextPos > target && speed > 0) || (nextPos < target && speed < 0)) 
+                queue.offer(new int[] { pos, speed > 0 ? -1 : 1, move + 1 });
         }
         return -1;
     }
